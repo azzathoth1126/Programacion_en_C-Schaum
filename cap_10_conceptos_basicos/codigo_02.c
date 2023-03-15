c15951b188936e2d43660966b437516a965148db
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-15 02:19:59 +0000

    complilado

--- cap_10_conceptos_basicos/codigo_02.c
+++ cap_10_conceptos_basicos/codigo_02.c
@@ -2,7 +2,14 @@
 
 int main(){
 
-    
+    int u1, u2;
+    int v = 3;
+    int *pv;
+
+    u1 = 2 * (v+5); // 2(3+5)
+    pv = &v; //3
+    u2 = 2 * (*pv+5);// 2(3*5)
+    printf("\nu1 = %d    u2 = %d\n\n", u1, u2);    
 
     return 0;
 }
